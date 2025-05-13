e9173f912366c20e437e5f51d38c2ed16bc732a5
Sixten Paulsson <sixtenpaulsson@gmail.com>
2025-05-13 13:00:02 +0200

    visual studio ändrar hur koden ser ut

--- lib/widgets/product_tile.dart
+++ lib/widgets/product_tile.dart
@@ -24,11 +24,14 @@ class ProductTile extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(8.0), // Add padding inside the card
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start, // Align content to the left
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Align content to the left
               children: [
                 // Product Image with Rounded Edges
                 ClipRRect(
-                  borderRadius: BorderRadius.circular(8), // Rounded edges for the image
+                  borderRadius: BorderRadius.circular(
+                    8,
+                  ), // Rounded edges for the image
                   child: SizedBox(
                     width: 90,
                     height: 90,
@@ -44,16 +47,14 @@ class ProductTile extends StatelessWidget {
                     fontWeight: FontWeight.bold,
                   ),
                   maxLines: 2, // Limit to 2 lines
-                  overflow: TextOverflow.ellipsis, // Add ellipsis if text overflows
+                  overflow:
+                      TextOverflow.ellipsis, // Add ellipsis if text overflows
                 ),
                 const SizedBox(height: 4), // Spacing between name and price
                 // Product Price
                 Text(
                   '${product.price.toStringAsFixed(2)} kr', // Display price with 2 decimal places
-                  style: const TextStyle(
-                    fontSize: 14,
-                    color: Colors.black,
-                  ),
+                  style: const TextStyle(fontSize: 14, color: Colors.black),
                 ),
                 const SizedBox(height: 5), // Spacing between price and buy card
                 // Buy Card
@@ -76,9 +77,10 @@ class ProductTile extends StatelessWidget {
     var iMat = Provider.of<ImatDataHandler>(context, listen: false);
     var isFavorite = iMat.isFavorite(product);
 
-    var icon = isFavorite
-        ? Icon(Icons.star, color: Colors.orange)
-        : Icon(Icons.star_border, color: Colors.orange);
+    var icon =
+        isFavorite
+            ? Icon(Icons.star, color: Colors.orange)
+            : Icon(Icons.star_border, color: Colors.orange);
 
     return IconButton(
       onPressed: () {
