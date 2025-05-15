4c2c0f2257a6150078b278bf7e0f2168d65b6efa
Sixten Paulsson <sixtenpaulsson@gmail.com>
2025-05-15 11:40:13 +0200

    changed primary font to lexend

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'iMat Demo',
-      theme: ThemeData(colorScheme: AppTheme.colorScheme),
+      theme: ThemeData(colorScheme: AppTheme.colorScheme, fontFamily: 'Lexend'),
       home: const MainView(),
     );
   }
